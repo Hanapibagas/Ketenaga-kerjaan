1738ac91b53b8b064bbe3c1bd3d7bac1b6809e0e
Hanapibagas <hanafibagas4@gmail.com>
2023-06-02 09:25:10 +0800

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -38,7 +38,7 @@ Login
                             <div class="col-md-6">
                                 <input id="password" type="password"
                                     class="form-control @error('password') is-invalid @enderror" name="password"
-                                    required autocomplete="current-password">
+                                    autocomplete="current-password">
 
                                 @error('password')
                                 <span class="invalid-feedback" role="alert">
